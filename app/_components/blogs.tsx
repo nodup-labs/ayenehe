66e5281522a8efb0710537ade8a5ec772e4340d0
ErfanKashef <129071476+ErfanKashef@users.noreply.github.com>
2025-12-29 11:19:50 +0330

    feat:remove deta blog and use blogdeta and map

--- app/_components/blogs.tsx
+++ app/_components/blogs.tsx
@@ -4,44 +4,11 @@ import "swiper/css/navigation";
 import "swiper/css/pagination";
 import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import { Swiper, SwiperSlide } from "swiper/react";
+import Link from "next/link";
 import CardBlog from "./CardBlog";
+import { blogPosts } from "@/lib/blogData";
 
 const Blogs = () => {
-  const blogs = [
-    {
-      category: "مدرسه الماس",
-      image: "/image/cover-3.webp",
-      title: "آماده‌سازی آزمون ورود به المپیاد",
-      description:
-        "آماده سازی بیش از دویست نفر از دانش آموزان استعداد های درخشان کمیته امداد امام خمینی برای شرکت در ازمون المپیاد توسط استارتاپ مدرسه الماس ",
-      date: "2025-12-07",
-    },
-    {
-      category: "سامانه ماه",
-      image: "/image/cover-3.webp",
-      title: "مسئولیت اجتماعی هوشمند",
-      description:
-        "طراحی بستر هوشمند تأمین مالی استارتاپ‌های تحت حمایت خانه خلاق  آینه با استفاده از مسئولیت اجتماعی شرکت‌ها توسط استارتاپ ماه",
-      date: "2025-12-06",
-    },
-    {
-      category: "خانه نگارین",
-      image: "/image/cover-3.webp",
-      title: "محل تجمع هنرمندان صنایع دستی",
-      description:
-        "طراحی بازار تخصصی فروش محصولات صنایع دستی با رویکرد صادرات‌محور توسط استارتاپ خانه نگارین",
-      date: "2025-12-05",
-    },
-    {
-      category: "سامانه حنا",
-      image: "/image/cover-3.webp",
-      title: "حمایت‌های نوآورانه اجتماعی",
-      description:
-        "طراحی بازار هوشمند با هدف تأمین کالاهای اساسی برای خانواده‌های کم‌برخوردار توسط استارتاپ حنا",
-      date: "2025-12-04",
-    },
-  ];
-
   return (
     <div dir="rtl" className="pt-8">
       <Swiper
@@ -55,15 +22,19 @@ const Blogs = () => {
         autoplay={{ delay: 5000 }}
         className="w-full"
       >
-        {blogs.map((blog, idx) => (
-          <SwiperSlide key={blog.title || idx}>
-            <CardBlog
-              category={blog.category}
-              image={blog.image}
-              title={blog.title}
-              description={blog.description}
-              date={blog.date}
-            />
+        {blogPosts.map((blog) => (
+          <SwiperSlide key={blog.id}>
+            <Link href={`/blog/${blog.id}`} className="block">
+              <div className="h-full">
+                <CardBlog
+                  category={blog.category}
+                  image={blog.image}
+                  title={blog.title}
+                  description={blog.description}
+                  date={blog.date}
+                />
+              </div>
+            </Link>
           </SwiperSlide>
         ))}
       </Swiper>
